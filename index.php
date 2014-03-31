b2a34cda3ce376a1bb9d2d2f19bb0ad662a4db34
Steve Wozniak <stephan.wozniak@gmail.com>
2014-03-31 01:34:51 -0400

    Checking in base site

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>FEST 13 | OCT 31, NOV 1 &amp; 2, 2014 | GAINESVILLE, FL || PRE-FEST 2 | OCT 29 &amp; 30, 2014 | YBOR (TAMPA), FL</title>
+    <meta name="description" content="FEST 13 | OCT 31, NOV 1 &amp; 2, 2014 | GAINESVILLE, FL || PRE-FEST 2 | OCT 29 &amp; 30, 2014 | YBOR (TAMPA), FL">
+	<meta name="keywords" content="the fest, fest 13, fest, pre-fest no idea records, gainesville, ybor">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	
+	<link type="text/css" rel="stylesheet" href="/css/reset.css">
+	<link type="text/css" rel="stylesheet" href="/css/fesy.css">
+
+	<link href='http://fonts.googleapis.com/css?family=Oswald:400,700|Open+Sans:400,700' rel='stylesheet' type='text/css'>
+
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-48284793-1', 'thefestfl.com');
+		ga('send', 'pageview');
+	</script>
+</head>
+<body>
+    <div class="content-wrapper">
+        <div id="fest-splash-wrapper" class="splash-wrapper">
+            <img id="/img/teaser.png" alt="FEST 13 - OCT 31, NOV 1 &amp; 2, 2014 - GAINESVILLE, FL">
+        </div>
+        <div class="memo-wrapper">
+	        <div id="prefest-splash-wrapper" class="splash-wrapper">
+	            <img id="/img/prefest_teaser.png" alt="PRE-FEST 2 | OCT 29 &amp; 30, 2014 | YBOR (TAMPA), FL">
+	        </div>
+	        
+        	<hr />
+
+			<div id="fest-dates">
+	            <h1><strong>Fest 13</strong> | Oct 31, Nov 1 &amp; 2, 2014</h1>
+	            <h4>Gainesville, FL</h4>
+	            <h1><strong>Pre-Fest 2</strong> | Oct 29 &amp; 30, 2014</h1>
+	            <h4>Ybor (Tampa), FL</h4>
+            </div>
+
+        	<hr />
+
+	        <div id="old-news">
+	            <h5>NEW SITE COMING APRIL 1ST, 2014.</h5>
+	        </div>
+	        <div id="social">
+	            <span>SHOW US SOME LOVE!</span>
+	            <div id="social-icon-wrapper">
+	                <a target="_blank" href="https://www.facebook.com/pages/THE-FEST/135233613179048"><img src="/img/social/facebook-48-white.png" /></a>
+	                <a target="_blank" href="https://twitter.com/thefestfl"><img src="/img/social/twitter-48-white.png" /></a>
+	                <a target="_blank" href="http://instagram.com/thefestfl"><img src="/img/social/instagram-48-white.png" /></a>
+	                <a target="_blank" href="http://www.flickr.com/photos/thefestfl/"><img src="/img/social/flickr-48-white.png" /></a>
+	            </div>
+	        </div>
+        </div> 
+    </div>
+</body>
+</html>
